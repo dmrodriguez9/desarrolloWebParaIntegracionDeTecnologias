895bda373e3891700b3cd7caac8b468ced483fc1
dmrodriguez9 <dmrodriguez9@espe.edu.ec>
2023-05-26 18:26:14 -0500

    primer parcial

---
+++ Primer parcial/Clases/programasGuiasDeber/datosPrimitivos/datosPrimitivos.js
@@ -0,0 +1,11 @@
+// Ejemplo de números
+let numero = 10;
+console.log(numero);
+
+// Ejemplo de cadena de texto
+let texto = "Hola, mundo!";
+console.log(texto);
+
+// Ejemplo de valor booleano
+let booleano = true;
+console.log(booleano);
---
+++ Primer parcial/Clases/programasGuiasDeber/typeof/typeof.js
@@ -0,0 +1,7 @@
+let numero = 10;
+let texto = "Hola, mundo!";
+let booleano = true;
+
+console.log(typeof numero);   // Devuelve "number"
+console.log(typeof texto);    // Devuelve "string"
+console.log(typeof booleano); // Devuelve "boolean"
